029eba9a0f001f71907b7c5851717f56ff2d981a
anvilm <211anvil112@gmail.com>
2025-12-28 00:17:17 +0300

    feat(entypoint): add http entrypoint

--- entypoint/http.php
+++ entypoint/http.php
@@ -1,3 +1,23 @@
 <?php
 
 declare(strict_types=1);
+
+use Application\Kernel;
+use Mcp\Server\Transport\StreamableHttpTransport;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$psr17Factory = new Psr17Factory();
+
+$server = Kernel::createServer()->run(
+    new StreamableHttpTransport(
+        new ServerRequestCreator(
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+        )->fromGlobals()
+    )
+);
