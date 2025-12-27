3bde6bdd779ae7e2a809fe539c59f4752490d0e7
anvilm <211anvil112@gmail.com>
2025-12-27 23:45:34 +0300

    fix(bootstrappers): create server bootstrapper

--- app/Bootstrappers/ServerBootstrapper.php
+++ app/Bootstrappers/ServerBootstrapper.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace Application\Bootstrappers;
 
+use Application\Config\ApplicationConfig\ApplicationConfig;
+use Mcp\Server\Builder;
+
 final readonly class ServerBootstrapper
 {
-
+    public static function bootstrap(Builder $builder): void
+    {
+        $builder->setServerInfo(
+            ApplicationConfig::appName(),
+            ApplicationConfig::appDescription()
+        );
+    }
 }
