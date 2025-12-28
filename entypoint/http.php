2ad3463a9b1b40a59295f0ef5d5a488b1ea372c4
anvilm <211anvil112@gmail.com>
2025-12-28 11:47:51 +0300

    fix(entrypoint): add emitter to http entrypoint

--- entypoint/http.php
+++ entypoint/http.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Application\Kernel;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Mcp\Server\Transport\StreamableHttpTransport;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7Server\ServerRequestCreator;
@@ -11,13 +12,15 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 $psr17Factory = new Psr17Factory();
 
-$server = Kernel::createServer()->run(
-    new StreamableHttpTransport(
-        new ServerRequestCreator(
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory,
-        )->fromGlobals()
+new SapiEmitter()->emit(
+    Kernel::createServer()->run(
+        new StreamableHttpTransport(
+            new ServerRequestCreator(
+                $psr17Factory,
+                $psr17Factory,
+                $psr17Factory,
+                $psr17Factory,
+            )->fromGlobals()
+        )
     )
 );
