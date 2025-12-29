4b4ed19e2e0f0825791f18fc1a7a03b82d1068ac
anvilm <211anvil112@gmail.com>
2025-12-30 00:47:59 +0300

    refactor(tools): remove test log from example tool

--- app/Tools/ExampleTool.php
+++ app/Tools/ExampleTool.php
@@ -5,15 +5,9 @@ declare(strict_types=1);
 namespace Application\Tools;
 
 use Mcp\Capability\Attribute\McpTool;
-use Psr\Log\LoggerInterface;
 
 final class ExampleTool
 {
-    public function __construct(LoggerInterface $logger)
-    {
-        $logger->error('Example tool');
-    }
-
     #[McpTool("example_tool", "Just example tool")]
     public function handle(string $name): string
     {
